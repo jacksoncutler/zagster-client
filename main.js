54409291564a9af7b23f800b04e1e3f49513b223
Jackson <jackson.cutler@gmail.com>
2019-11-18 10:30:23 -0800

    Graph & Request
    inserted placeholder image for bar graph & initial idea for zagster request (commented out for now)

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 //alert("Hello, World!")
 
-
 $(helloWorld)
 
 function helloWorld() {
     alert("Hello, World! (but with jQuery)")
 }
+
+/* request to '/rides/count/per_hour' will look a little something like this:
+
+app.get('/rides/count/per_hour', (request, response) => {
+	const pool = new Pool({
+		connectionString: DATABASE_URL
+	})
+	
+	pool.query('SELECT * FROM rides', (err, results) => {
+		response.send(results.rows[0])
+		pool.end()
+	})
+})
+
+*/
