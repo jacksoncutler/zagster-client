c6e62c963bc246bb6dbdfbb4307de799d199c29a
Jackson <jackson.cutler@gmail.com>
2019-11-20 13:45:25 -0800

    chart.js (in progress)
    added chart template - need to figure out how to pull data from github repo for years, months, etc.

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-//alert("Hello, World!")
+// Exercise 14
 
-$(helloWorld)
+/* $(helloWorld)
 
 function helloWorld() {
     alert("Hello, World! (but with jQuery)")
+} */
+
+
+// Exercise 15
+
+const BASE_URL = "https://zagster-service.herokuapp.com"
+
+$(updateView)
+
+function updateView() {
+  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
 }
 
-/* request to '/rides/count/per_hour' will look a little something like this:
-
-app.get('/rides/count/per_hour', (request, response) => {
-	const pool = new Pool({
-		connectionString: DATABASE_URL
-	})
-	
-	pool.query('SELECT * FROM rides', (err, results) => {
-		response.send(results.rows[0])
-		pool.end()
-	})
-})
-
-*/
+function updateRideCount(data) {
+  numberOfRides = data.count
+  $("p#data").html(numberOfRides)
+}
