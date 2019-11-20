d9b6f1f0d76e724d83bd33598721775136238ea7
Jackson <jackson.cutler@gmail.com>
2019-11-20 13:54:59 -0800

    fixing master...

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 // Exercise 14
 
-<<<<<<< HEAD
 /* $(helloWorld)
-=======
-$(helloWorld)
->>>>>>> master
 
 function helloWorld() {
     alert("Hello, World! (but with jQuery)")
@@ -21,24 +17,7 @@ function updateView() {
   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
 }
 
-<<<<<<< HEAD
 function updateRideCount(data) {
   numberOfRides = data.count
   $("p#data").html(numberOfRides)
 }
-=======
-/* request to '/rides/count/per_hour' will look a little something like this:
-
-app.get('/rides/count/per_hour', (request, response) => {
-	const pool = new Pool({
-		connectionString: DATABASE_URL
-	})
-	
-	pool.query('SELECT * FROM rides', (err, results) => {
-		response.send(results.rows[0])
-		pool.end()
-	})
-})
-
-*/
->>>>>>> master
