1e5ef502bd5c6efa5bba2babd3abb7975b1c5e1d
Jackson <jackson.cutler@gmail.com>
2019-11-20 22:36:40 -0800

    change request
    request from rides/count/by_year

--- main.js
+++ main.js
@@ -11,13 +11,56 @@ function helloWorld() {
 
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
-$(updateView)
+$(updateGraph)
 
-function updateView() {
-  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+function updateGraph() {
+  $.getJSON(BASE_URL + "/rides/count/per_year" , updateRideCount)
 }
 
 function updateRideCount(data) {
-  numberOfRides = data.count
-  $("p#data").html(numberOfRides)
+  items = []
+  $.each(data, storeData)
+
+  // ORIGINAL
+  /* numberOfRides = data.count
+  $("p#data").html(numberOfRides) */
 }
+
+function storeData(key, value) {
+  items.push(' ' + key + ':' + value)
+}
+
+
+$(initGraph)
+
+function initGraph() {
+var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        /* labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], */
+        labels: ['2016', '2017', '2018'],
+        datasets: [{
+            label: 'Zagster Bike Share',
+            backgroundColor: 'rgb(50, 200, 150)',
+            borderColor: 'rgb(50, 200, 150)',
+            /* data: [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 30] */
+            data: [50, 50, 50]
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+    });
+  }
