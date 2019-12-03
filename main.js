34c4397f56c5cc9ef1ee545e844d79e7f5b40397
Jackson <jackson.cutler@gmail.com>
2019-12-02 19:54:25 -0800

    name changes
    to variable & function names

--- main.js
+++ main.js
@@ -40,9 +40,9 @@ $(per_month)
 // To graph the amount of zagster rentals by year
 // Not currently being used
 function per_year() {
-    $.getJSON(BASE_URL + "/rides/count/per_year" , get_year_counts)
+    $.getJSON(BASE_URL + "/rides/count/per_year" , get_yearly_counts)
 }
-function get_year_counts(data) {
+function get_yearly_counts(data) {
     years = []
     counts = []
     $.each(data, function(key,value) {
@@ -55,15 +55,16 @@ function get_year_counts(data) {
 
 // To graph the amount of zagster rentals by month for each year
 function per_month() {
-    $.getJSON(BASE_URL + "/rides/count/per_month" , get_month_counts)
+    $.getJSON(BASE_URL + "/rides/count/per_month" , get_monthly_counts)
 }
-function get_month_counts(data) {
+function get_monthly_counts(data) {
     years = []
-    years_data = []
+    yearly_data = []
+    
     // Extract external key/value pairs from {year:[array-of-{months:counts}]} format
     $.each(data, function(key,value) {
         years.push(key)
-        years_data.push(value) // NOTE: "value" is an array, making years_data a 2D array
+        yearly_data.push(value) // NOTE: "value" is an array, making yearly_data a 2D array
     })
     
     months = [] // array of x-values passed to graph
@@ -72,10 +73,12 @@ function get_month_counts(data) {
     // Walk thru each year
     for (i=0; i < years.length; i++) {
         temp_counts = []
+        
         // Walk thru each month within the year
-        for (j=0; j < years_data[i].length; j++) {
+        for (j=0; j < yearly_data[i].length; j++) {
+            
             // Extract key/value pairs from {month:count} format
-            $.each(years_data[i][j], function(key,value) {
+            $.each(yearly_data[i][j], function(key,value) {
                 months.push(MONTH_LIST[key-1] + ' (' + years[i] + ')') // use name of month & include the year
                 counts.push(value)
                 temp_counts.push(value)
@@ -84,10 +87,10 @@ function get_month_counts(data) {
         console.log('Ride count per-month during ' + years[i] + ': ', temp_counts)
     }
     console.log('Months:', months, 'Rides-per-month:', counts)
-    $(setGraph(months, counts))
+    $(createGraph(months, counts))
 }
 
-function setGraph(x_list, y_list) {
+function createGraph(x_list, y_list) {
 var ctx = document.getElementById('myChart').getContext('2d');
     var chart = new Chart(ctx, {
     // The type of chart we want to create
